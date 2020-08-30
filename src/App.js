c292ba0e6ab8f8b01ea00a348031f7be6258dcb7
FoxcroftN <neilfoxcroftpro@gmail.com>
2020-08-30 14:02:15 +0200

    initialized temp page
    created temp page as placeholder while developing the final product. page hosted on aws completed and continues pipeline implemented. www.neilfoxcroft.com and neilfoxcroft.com issues needs to be resolved still

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 //main web application
 
 import React from 'react';
-import logo from './logo.svg';
-//import './App.css';
+import './App.css';
+import HeaderBar from "./HeaderBar.js"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderBar />
     </div>
   );
 }
---
+++ src/HeaderBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HeaderBar() {
+  return (
+    <div>
+      <h1>Neil Foxcroft</h1>
+      <p>Personal Technical CV coming soon...</p>
+    </div>
+  );
+}
+
+export default HeaderBar;
